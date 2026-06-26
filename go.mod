08eb95558b54ceb347e792155840918221712c08
xpwu <diodexp@foxmail.com>
2026-06-26 09:47:32 +0800

    update dep

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/xpwu/go-db-mongo
 go 1.16
 
 require (
-	github.com/xpwu/go-cmd v0.1.0
 	github.com/xpwu/go-log v0.1.0
+	github.com/xpwu/go-x v0.3.0
 	go.mongodb.org/mongo-driver v1.8.4
 )
--- mongodb/mongocache/client.go
+++ mongodb/mongocache/client.go
@@ -1,168 +1,165 @@
 package mongocache
 
 import (
-  "context"
-  "fmt"
-  "github.com/xpwu/go-cmd/exe"
-  "github.com/xpwu/go-db-mongo/mongodb/filter"
-  "github.com/xpwu/go-db-mongo/mongodb/index"
-  "github.com/xpwu/go-db-mongo/mongodb/tagparser"
-  "github.com/xpwu/go-db-mongo/mongodb/updater"
-  "github.com/xpwu/go-log/log"
-  "go.mongodb.org/mongo-driver/bson"
-  "go.mongodb.org/mongo-driver/bson/bsoncodec"
-  "go.mongodb.org/mongo-driver/bson/bsonrw"
-  "go.mongodb.org/mongo-driver/mongo"
-  "go.mongodb.org/mongo-driver/mongo/options"
-  "go.mongodb.org/mongo-driver/mongo/readpref"
-  "reflect"
-  "sync"
-  "time"
+	"context"
+	"fmt"
+	"github.com/xpwu/go-db-mongo/mongodb/filter"
+	"github.com/xpwu/go-db-mongo/mongodb/index"
+	"github.com/xpwu/go-db-mongo/mongodb/tagparser"
+	"github.com/xpwu/go-db-mongo/mongodb/updater"
+	"github.com/xpwu/go-log/log"
+	"github.com/xpwu/go-x/exe"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsoncodec"
+	"go.mongodb.org/mongo-driver/bson/bsonrw"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"reflect"
+	"sync"
+	"time"
 )
 
 var (
-  clients = sync.Map{}
+	clients = sync.Map{}
 )
 
 type Client struct {
-  *mongo.Client
-  maxConn uint64
-  id      string
+	*mongo.Client
+	maxConn uint64
+	id      string
 }
 
 func (c *Client) Disconnect(ctx context.Context) error {
-  // nothing to do
-  return nil
+	// nothing to do
+	return nil
 }
 
 // 一个client可能在很多地方使用，调用此函数时，所有基于此client的请求都将失败
 func (c *Client) DisconnectForce(ctx context.Context) error {
-  clients.Delete(c.id)
+	clients.Delete(c.id)
 
-  return c.Client.Disconnect(ctx)
+	return c.Client.Disconnect(ctx)
 }
 
 func newClient(config *Config) (*Client, error) {
-  client, err := mongo.NewClient(
-    // 根据底层逻辑，顺序很重要，取的都是最后设置的值
-    // 默认值先设置
-    options.Client().
-      SetAppName(exe.Exe.Name).
-      SetConnectTimeout(5 * time.Second).
-      SetServerSelectionTimeout(5 * time.Second).
-      SetSocketTimeout(5 * time.Second).
-      SetRetryWrites(true).
-      SetMaxConnIdleTime(3 * time.Minute).
-      SetReadPreference(readpref.SecondaryPreferred()).
-      SetMinPoolSize(1).
+	client, err := mongo.NewClient(
+		// 根据底层逻辑，顺序很重要，取的都是最后设置的值
+		// 默认值先设置
+		options.Client().
+			SetAppName(exe.Name).
+			SetConnectTimeout(5 * time.Second).
+			SetServerSelectionTimeout(5 * time.Second).
+			SetSocketTimeout(5 * time.Second).
+			SetRetryWrites(true).
+			SetMaxConnIdleTime(3 * time.Minute).
+			SetReadPreference(readpref.SecondaryPreferred()).
+			SetMinPoolSize(1).
 
-      // 然后是URI 携带的参数，最后是配置中明确指明的设置
-      ApplyURI(config.URI).
-      SetMaxPoolSize(config.MaxConn).
-      SetAuth(options.Credential{
-        Username:    config.User,
-        Password:    config.Password,
-        PasswordSet: true,
-      }).
+			// 然后是URI 携带的参数，最后是配置中明确指明的设置
+			ApplyURI(config.URI).
+			SetMaxPoolSize(config.MaxConn).
+			SetAuth(options.Credential{
+				Username:    config.User,
+				Password:    config.Password,
+				PasswordSet: true,
+			}).
+			SetRegistry(defaultRegister),
+	)
 
-      SetRegistry(defaultRegister),
-  )
+	if err != nil {
+		return nil, err
+	}
 
-  if err != nil {
-    return nil, err
-  }
-
-  return &Client{
-    Client:  client,
-    id:      config.id(),
-    maxConn: config.MaxConn,
-  }, nil
+	return &Client{
+		Client:  client,
+		id:      config.id(),
+		maxConn: config.MaxConn,
+	}, nil
 }
 
 func MustGet(ctx context.Context, config Config) *Client {
-  r, err := Get(ctx, config)
-  if err != nil {
-    panic(err)
-  }
+	r, err := Get(ctx, config)
+	if err != nil {
+		panic(err)
+	}
 
-  return r
+	return r
 }
 
 func Get(ctx context.Context, config Config) (ret *Client, err error) {
-  _, logger := log.WithCtx(ctx)
-  id := config.id()
+	_, logger := log.WithCtx(ctx)
+	id := config.id()
 
-  c, ok := clients.Load(id)
-  if ok {
-    ret = c.(*Client)
-    if ret.maxConn < config.MaxConn {
-      logger.Warning(fmt.Sprintf("return cache Client<%s>, whose maxCon(%d) is less than expected(%d)",
-        config.URI, ret.maxConn, config.MaxConn))
-    }
-    return
-  }
+	c, ok := clients.Load(id)
+	if ok {
+		ret = c.(*Client)
+		if ret.maxConn < config.MaxConn {
+			logger.Warning(fmt.Sprintf("return cache Client<%s>, whose maxCon(%d) is less than expected(%d)",
+				config.URI, ret.maxConn, config.MaxConn))
+		}
+		return
+	}
 
-  nc, err := newClient(&config)
-  if err != nil {
-    return
-  }
+	nc, err := newClient(&config)
+	if err != nil {
+		return
+	}
 
-  c, ok = clients.LoadOrStore(id, nc)
-  ret = c.(*Client)
-  // new client
-  if !ok {
-    // 连接使用新的ctx
-    err = ret.Connect(context.Background())
-  }
-  if err != nil {
-    return
-  }
+	c, ok = clients.LoadOrStore(id, nc)
+	ret = c.(*Client)
+	// new client
+	if !ok {
+		// 连接使用新的ctx
+		err = ret.Connect(context.Background())
+	}
+	if err != nil {
+		return
+	}
 
-  return
+	return
 }
 
 var structCodec, _ = bsoncodec.NewStructCodec(
-  bsoncodec.StructTagParserFunc(tagparser.StructTagParser))
+	bsoncodec.StructTagParserFunc(tagparser.StructTagParser))
 
 var defaultRegister = bson.NewRegistryBuilder().
-  RegisterDefaultDecoder(reflect.Struct, structCodec).
-  RegisterDefaultEncoder(reflect.Struct, structCodec).
+	RegisterDefaultDecoder(reflect.Struct, structCodec).
+	RegisterDefaultEncoder(reflect.Struct, structCodec).
+	RegisterTypeEncoder(reflect.TypeOf((*updater.Updater)(nil)).Elem(),
+		bsoncodec.ValueEncoderFunc(func(ec bsoncodec.EncodeContext,
+			vw bsonrw.ValueWriter, val reflect.Value) error {
 
-  RegisterTypeEncoder(reflect.TypeOf((*updater.Updater)(nil)).Elem(),
-    bsoncodec.ValueEncoderFunc(func(ec bsoncodec.EncodeContext,
-      vw bsonrw.ValueWriter, val reflect.Value) error {
+			v := val.Interface().(updater.Updater).ToBsonM()
+			enc, err := ec.LookupEncoder(reflect.TypeOf(v))
+			if err != nil {
+				return err
+			}
 
-      v := val.Interface().(updater.Updater).ToBsonM()
-      enc, err := ec.LookupEncoder(reflect.TypeOf(v))
-      if err != nil {
-        return err
-      }
+			return enc.EncodeValue(ec, vw, reflect.ValueOf(v))
+		})).
+	RegisterTypeEncoder(reflect.TypeOf((*filter.Filter)(nil)).Elem(),
+		bsoncodec.ValueEncoderFunc(func(ec bsoncodec.EncodeContext,
+			vw bsonrw.ValueWriter, val reflect.Value) error {
 
-      return enc.EncodeValue(ec, vw, reflect.ValueOf(v))
-    })).
-  RegisterTypeEncoder(reflect.TypeOf((*filter.Filter)(nil)).Elem(),
-    bsoncodec.ValueEncoderFunc(func(ec bsoncodec.EncodeContext,
-      vw bsonrw.ValueWriter, val reflect.Value) error {
+			v := val.Interface().(filter.Filter).ToBsonD()
+			enc, err := ec.LookupEncoder(reflect.TypeOf(v))
+			if err != nil {
+				return err
+			}
 
-      v := val.Interface().(filter.Filter).ToBsonD()
-      enc, err := ec.LookupEncoder(reflect.TypeOf(v))
-      if err != nil {
-        return err
-      }
+			return enc.EncodeValue(ec, vw, reflect.ValueOf(v))
+		})).
+	RegisterTypeEncoder(reflect.TypeOf((*index.Key)(nil)).Elem(),
+		bsoncodec.ValueEncoderFunc(func(ec bsoncodec.EncodeContext,
+			vw bsonrw.ValueWriter, val reflect.Value) error {
 
-      return enc.EncodeValue(ec, vw, reflect.ValueOf(v))
-    })).
-  RegisterTypeEncoder(reflect.TypeOf((*index.Key)(nil)).Elem(),
-    bsoncodec.ValueEncoderFunc(func(ec bsoncodec.EncodeContext,
-      vw bsonrw.ValueWriter, val reflect.Value) error {
+			v := val.Interface().(index.Key).ToBsonD()
+			enc, err := ec.LookupEncoder(reflect.TypeOf(v))
+			if err != nil {
+				return err
+			}
 
-      v := val.Interface().(index.Key).ToBsonD()
-      enc, err := ec.LookupEncoder(reflect.TypeOf(v))
-      if err != nil {
-        return err
-      }
-
-      return enc.EncodeValue(ec, vw, reflect.ValueOf(v))
-    })).
-
-  Build()
+			return enc.EncodeValue(ec, vw, reflect.ValueOf(v))
+		})).
+	Build()
