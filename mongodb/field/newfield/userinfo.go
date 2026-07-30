c2a04fd64fe9382d8eae543c12fc245870f7a864
xpwu <diodexp@foxmail.com>
2026-07-30 13:57:06 +0800

    Update userinfo.go

--- mongodb/field/newfield/userinfo.go
+++ mongodb/field/newfield/userinfo.go
@@ -1,16 +1,21 @@
 package newfield
 
-import "github.com/xpwu/go-db-mongo/mongodb/geojson"
+import (
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
 
 type Wx struct {
-	Age  geojson.Point
+	Age  bson.Decimal128
 	Time *int
 }
 
 type UserInfo struct {
 	Login int
 	Pass  []int
-	//Wx    Wx
-	//Ws    []Wx
-	Pass2 []int16
+	Wx    Wx
+	Ws    []Wx
+	Pass2 [][]int16
+	InWx  Wx `bson:"inWx,inline"`
 }
+
+//var filter2 = UserInfoDoc.AgeF().
